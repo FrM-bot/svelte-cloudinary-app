96174697531c7b96da327895b3756d02a2aca9b0
Franco M <damianmaciel0@gmail.com>
2023-03-06 18:54:41 -0300

    Correct button "Confirm Download"

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,23 +4,17 @@
 	import Herader from '$lib/Herader.svelte'
 	import '../styles/app.css'
 	import { imageToEdit } from '../store/image'
-	import { setLocalStorageValue } from '../utils/localStorage'
-	import { LOCAL_STORAGE_KEYS } from '../types/LocalStorage'
-	import { goto } from '$app/navigation'
 	onMount(() => {
-		window.addEventListener('beforeunload', function (e) {
-			const confirmationMessage = 'o/'
-			e.returnValue = confirmationMessage
-			return confirmationMessage
+		window.addEventListener('beforeunload', (event) => {
+			event.preventDefault()
+			return (event.returnValue = '')
 		})
-		window.onunload = function () {
-			if (!navigator.sendBeacon) return
+		window.onunload = () => {
+			if (!window.navigator.sendBeacon) return
 
 			if ($imageToEdit?.publicId.length > 0) {
 				const url = `/api/${$imageToEdit.publicId}`
-				setLocalStorageValue(LOCAL_STORAGE_KEYS.IMAGE, null)
 				navigator.sendBeacon(url, JSON.stringify({ publicId: $imageToEdit?.publicId }))
-				goto('/')
 			}
 		}
 	})
