b391a182e8494bb654c37b7c006d4b28ec5ca0d7
Franco M <82059717+FrM-bot@users.noreply.github.com>
2023-03-08 04:18:16 -0300

    Remove import in Effect (#13)
    * Add download image an page 404
    
    * Add font Roboto Mono
    
    * Fix error with images
    
    * Fix error to remove image
    
    * Correct button "Confirm Download"
    
    * Add new effects ad ajust
    
    * Fix vitest error
    
    * Remove import in effect

--- src/lib/utils/Effect.ts
+++ src/lib/utils/Effect.ts
@@ -18,12 +18,6 @@ import {
 	assistColorBlind
 } from '@cloudinary/url-gen/actions/effect'
 
-import {
-	Actions
-} from '@cloudinary/url-gen'
-
-// console.log(Actions.Delivery.density(2))
-
 import type { ConditionTypes } from '$lib/types/Effect'
 export const EffectCloudinary = {
 	pixelate,
