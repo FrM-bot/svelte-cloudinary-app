8b8229ca51127573e7ae4d3161bb8f80b25d61cb
Franco M <82059717+FrM-bot@users.noreply.github.com>
2023-03-09 02:46:27 -0300

    Fix urls cloudinary (#15)
    * Add download image an page 404
    
    * Add font Roboto Mono
    
    * Fix error with images
    
    * Fix error to remove image
    
    * Correct button "Confirm Download"
    
    * Add new effects ad ajust
    
    * Fix vitest error
    
    * Remove import in effect
    
    * Fix error to remove image
    
    * Fix url cloudinary

--- src/lib/services/URL.ts
+++ src/lib/services/URL.ts
@@ -1,6 +1,10 @@
+const  { VITE_CLOUD_NAME : cloudName} = import.meta.env
+
+const baseUrl = `https://api.cloudinary.com/v1_1/${cloudName}`
+
 export const URL = {
-	upload: 'https://api.cloudinary.com/v1_1/dxkry5nri/image/upload',
-    destroy: 'https://api.cloudinary.com/v1_1/dxkry5nri/image/destroy',
-    destroyByToken: 'https://api.cloudinary.com/v1_1/dxkry5nri/delete_by_token',
-    download: 'https://api.cloudinary.com/v1_1/dxkry5nri/download_backup'
+	upload: `${baseUrl}/image/upload`,
+    destroy: `${baseUrl}/image/destroy`,
+    destroyByToken: `${baseUrl}/delete_by_token`,
+    download: `${baseUrl}/download_backup`
 }
